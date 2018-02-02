58f173ecf34503d0cd4a18773cfc81b15c3dd996
heyushuo <1007561607@qq.com>
2018-02-02 11:33:46 +0800

    init

--- src/js/router.js
+++ src/js/router.js
@@ -1,27 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import Index from './index'
-
-import {Router,Route,hashHistory} from 'react-router'
-import ComponentList from './components/list'
-import ComponentDetail from "./components/detail";
-
 
 export default class Root extends React.Component{
 	render(){
 		return (
-			//这里替换之前的index。变成程序的入口
-			<Router history={hashHistory} >
-				<Route component={Index} path="/" >
-					<Route component={ComponentDetail} path="detail" ></Route>
-				</Route>
-				<Route component={ComponentList} path="list/:id"></Route>
-			</Router>
+			<div>
+				<h1>我是项目的入口文件</h1>
+			</div>
 		)
 	}
 }
 
 ReactDom.render(
 	<Root />,
-	document.getElementById("example")
+	document.getElementById("mainContainer")
 )

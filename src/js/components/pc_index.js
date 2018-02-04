967d712a598b298db6df5f593d33ce428c5ee640
heyushuo <1007561607@qq.com>
2018-02-04 22:31:03 +0800

    fix

--- src/js/components/pc_index.js
+++ src/js/components/pc_index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Row, Col } from 'antd';
 import PCHeader from './pc_header'
+import PCFooter from './pc_footer';
 export default class PCIndex extends React.Component{
 	render(){
 		return (
 			<div>
-				<PCHeader />
+				<PCHeader></PCHeader>
+				<PCFooter></PCFooter>
 			</div>
 		)
 	}

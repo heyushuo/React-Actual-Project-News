5ff2106484517933380d655661f13a01a57b50ba
heyushuo <1007561607@qq.com>
2018-02-05 09:49:23 +0800

    babel

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -1,17 +1,48 @@
 import React from 'react'
-import { Row, Col ,Menu, Icon} from 'antd';
+import {
+	Row,
+	Col ,
+	Menu,
+	Icon,
+	Modal,
+	Button
+		} from 'antd';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 export default class PCHeader extends React.Component{
 	constructor(){
 		super();
-		this.handleClick=this.handleClick.bind(this)
+		this.handleClick=this.handleClick.bind(this);
+		this.showModal=this.showModal.bind(this);
+		this.handleOk=this.handleOk.bind(this);
+		this.handleCancel=this.handleCancel.bind(this);
+
 		this.state={
-			current:"top"
+			current:"top",
+			visible:false
+
 		}
 
 	}
-
+	//弹出框开始
+	const showModal=() => {
+		this.setState({
+			visible: true,
+		});
+	}
+	const handleOk=(e) => {
+		console.log(e);
+		this.setState({
+			visible: false,
+		});
+	}
+	const handleCancel=(e) => {
+		console.log(e);
+		this.setState({
+			visible: false,
+		});
+	}
+	//弹出框的结束
 	handleClick(e) {
 		this.setState({
 			current: e.key,
@@ -60,6 +91,18 @@ export default class PCHeader extends React.Component{
 								<Icon type="appstore"/>时尚
 							</Menu.Item>
 						</Menu>
+						{/*弹出框的部分代码*/}
+						<Modal
+							title="Basic Modal"
+							visible={this.state.visible}
+							onOk={this.handleOk}
+							onCancel={this.handleCancel}
+						>
+							<p>Some contents...</p>
+							<p>Some contents...</p>
+							<p>Some contents...</p>
+						</Modal>
+						{/*弹出框的部分代码结束*/}
 					</Col>
 					<Col span={2}></Col>
 					<Col span={2}></Col>
